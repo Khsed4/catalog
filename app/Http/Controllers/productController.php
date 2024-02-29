1e1f4bbc6695c142230d9ccda27a408d44df2f02
Sed AKH <kh.seddiqi14@gmail.com>
2024-02-29 18:24:39 +0430

    changes of Islamic items

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -156,9 +156,9 @@ class productController extends Controller
         if ($cataloge_id == 'All' &&  $category_id != '0') {
             $products = Product::where('out_of_stock', '!=', 0)->orderBy('category_id', 'desc')->get();
         } elseif ($category_id == '0')
-            $products = Product::where('cataloge_id', $cataloge_id)->where('out_of_stock', '!=', 0)->orderBy('category_id', 'desc')->get();
+            $products = Product::where('cataloge_id', $cataloge_id)->where('out_of_stock', '!=', 0)->orderBy('category_id', 'desc')->paginate(8)->get();
         else
-            $products = Product::where('cataloge_id', $cataloge_id)->where('category_id', $category_id)->orderBy('category_id', 'desc')->get();
+            $products = Product::where('cataloge_id', $cataloge_id)->where('category_id', $category_id)->orderBy('category_id', 'desc')->paginate(8)->get();
         $data['products'] = $products;
 
         if ($print_type == '2') {
--- resources/views/print/two-items.blade.php
+++ resources/views/print/two-items.blade.php
@@ -16,51 +16,71 @@
 </head>
 
 <body>
-    <header style="display:inline';overflow:inherit ">
-
-        <p style="margin-top:3px">Unique Natural LLC</p>
-
-
-    </header>
 
     <div class="page">
-
         <table>
-
-
             @for ($i = 0; $i < count($products); $i += 2)
                 <tbody>
                     <tr>
                         <td>
-                            <div class="card">
-                                <img class="image"
-                                    src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/' . $products[$i]->image))) }}">
-                                <P class="name">{{ $products[$i]->name }}</P>
-                                <div class="mainDiv">
+                            <div class="card ">
+                                <div>
+                                    <P class="name">{{ $products[$i]->name }}</P>
+                                </div>
+                                <div>
+                                    <img class="image"
+                                        src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/' . $products[$i]->image))) }}"
+                                        alt="Denim Jeans" />
+                                </div>
 
-                                    <span class="price">Price : {{ $products[$i]->price }} $</span>
-                                    <span class="SKU">SKU: {{ $products[$i]->SKU }}</span>
+                                <div class="mainDiv">
+                                    @if ($products[$i]->actual_price > 0)
+                                        <span class="price">
+                                            <span class="off-price">
+                                                ${{ $products[$i]->price }}
+                                            </span>
+                                            <span>${{ $products[$i]->actual_price }}</span>
+                                        </span>
+                                    @else
+                                        <span class="price">Price:${{ $products[$i]->price }}</span>
+                                    @endif
+
+                                    <span class="item-number itemsIn"> Code:
+                                        {{ $products[$i]->item_number }}</span>
+                                    <span class="SKU ">SKU: {{ $products[$i]->SKU }}</span>
 
                                 </div>
+
                             </div>
                         </td>
 
                         @if (isset($products[$i + 1]))
                             <td>
                                 <div class="card second">
-                                    <img class="image"
-                                        src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/' . $products[$i + 1]->image))) }}"
-                                        alt="Denim Jeans">
-                                    <P class="name">{{ $products[$i + 1]->name }}</P>
+                                    <div class="title">
+                                        <P class="name">{{ $products[$i + 1]->name }}</P>
+                                    </div>
+                                    <div class="image-div">
+                                        <img class="image"
+                                            src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/' . $products[$i + 1]->image))) }}"
+                                            alt="Denim Jeans" />
+
+                                    </div>
+
                                     <div class="mainDiv">
+                                        @if ($products[$i + 1]->actual_price > 0)
+                                            <span class="price">
+                                                <span class="off-price">
+                                                    ${{ $products[$i + 1]->price }}
+                                                </span>
+                                                <span>${{ $products[$i + 1]->actual_price }}</span>
+                                            </span>
+                                        @else
+                                            <span class="price">Price:${{ $products[$i + 1]->price }}</span>
+                                        @endif
 
-                                        <span class="price">Price : {{ $products[$i + 1]->price }} $</span>
-
-                                        {{-- <span>
-                                            <span class="item-number itemsIn"> Code :
-                                                {{ $products[$i + 1]->item_number }}</span>
-                                        </span> --}}
-
+                                        <span class="item-number itemsIn"> Code:
+                                            {{ $products[$i + 1]->item_number }}</span>
                                         <span class="SKU ">SKU: {{ $products[$i + 1]->SKU }}</span>
 
                                     </div>
